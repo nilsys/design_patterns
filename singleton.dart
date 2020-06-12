dc765dc0e02a9354b1cc483199af99c753c91b2f
unknown <ammaryaser293@gmail.com>
2020-06-12 23:45:59 +0200

    singleton pattern completed

--- singleton.dart
+++ singleton.dart
@@ -1,15 +1,28 @@
 library design_patterns;
 
 class Singleton {
+  /// instantiating a private constructor
   Singleton._();
-  static final _instance = Singleton._();
-  factory Singleton() {
-    print(_instance.hashCode);
-    return _instance;
-  }
+
+  static final staticSingleton = Singleton._();
+
+  static final _staticGetterSingleton = Singleton._();
+  static get staticGetterSingleton => _staticGetterSingleton;
+
+  static final _factorySingleton = Singleton._();
+  factory Singleton() => _factorySingleton;
 }
 
 void main() {
-  Singleton();
-  Singleton();
+  Singleton staticSingleton = Singleton.staticSingleton;
+  Singleton staticGetterSingleton = Singleton.staticGetterSingleton;
+  Singleton factorySingleton = Singleton();
+
+  // printing hash codes for all different singleton ways
+  print(staticSingleton.hashCode);
+  print(staticGetterSingleton.hashCode);
+  print(factorySingleton.hashCode);
+
+  // note that if you tried print the hash code of two objects
+  // of the same singleton method, they would be the same!
 }
